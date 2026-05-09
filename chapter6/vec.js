5d2a59b0f6fd3c1e12e0486e593a8eca3073ca69
Gustavo <7uic3codes@gmail.com>
2026-05-08 20:43:01 -0600

    Vector class, learned about getters and setters with this exercise, functions and classes

---
+++ chapter6/vec.js
@@ -0,0 +1,27 @@
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plus(vector) {
+        this.x += vector.x;
+        this.y += vector.y;
+        return this;
+    }   
+
+    minus(vector) {
+        this.x -= vector.x;
+        this.y -= vector.y;
+        return this;
+    }
+
+    get length() {
+        return Math.sqrt((this.x ** 2) + (this.y ** 2));
+    }
+}
+
+console.log(new Vec(1,2).plus(new Vec(2,3)));
+console.log(new Vec(1,2).minus(new Vec(2,3)));
+console.log(new Vec(3,4).length);
+
